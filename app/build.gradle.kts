6425ddf1ce1fbd44dc200f20e43196f458a9cf83
김진우 <jinwoo3661@naver.com>
2025-01-15 15:26:23 +0900

    chore: 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,11 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.android.junit5)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -48,6 +53,12 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.bundles.test)
+    implementation(libs.bundles.android)
+    implementation(libs.bundles.network)
+    implementation(libs.bundles.kotlin)
+    implementation(libs.bundles.google)
+    implementation(libs.googlke.service)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.hilt) apply false
 }
